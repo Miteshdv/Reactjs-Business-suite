cb513e6778c89cd3e6ea61c8f4754612aeaf4ace
Mitesh dave <Miteshdv@yahoo.co.in>
2016-04-25 14:26:56 +0530

    First Commit

---
+++ components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Container from 'react-layout-components';
+import Box from 'react-layout-components';
+import  'react-widgets/lib/less/react-widgets.less';
+
+class App extends React.Component {
+
+   
+   render() {
+      
+      return (
+
+      	 <Box width ="63.5%" height ={300} column>
+
+             <div>Test Suite</div>
+
+
+
+      	
+      	 </Box>
+         
+        
+      );
+   }
+}
+
+
+
+export default App
+
