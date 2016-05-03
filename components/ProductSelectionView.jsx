19d19251de70300004355663587b9fa682c4646c
Mitesh dave <Miteshdv@yahoo.co.in>
2016-05-03 20:02:20 +0530

    Added tab navigator and nerged grid combo

---
+++ components/ProductSelectionView.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import TreeView from './TreeView.jsx'
+
+import ProductListView from './ProductListComponent/ProductsListView.jsx';
+import  'react-widgets/lib/less/react-widgets.less';
+
+
+
+
+class ProductSelectionView extends React.Component { 
+
+  constructor() {
+     super()
+     var viewHeight = window.innerHeight;
+     this.state = {windowHeight:viewHeight};
+     window.onresize = this.resizeView.bind(this);
+   }
+
+   resizeView()
+   {  
+     this.setState({windowHeight:window.innerHeight})
+     
+   }
+
+  
+ 
+   render() {
+    var data = [];
+    
+    for(var i = 1 ; i < 51;i++)
+    {
+      var obj = {};
+      obj.url = "http://fakeimg.pl/100/?text=img"+i;
+      obj.name = "img"+i;      
+      obj.id = i;      
+      data.push(obj);
+
+    }
+      return (
+      	 
+           
+               <div  style={{float:"none",width:"100%"}}>   
+                <div  style= {{width:"14.5%" ,height:this.state.windowHeight*0.86,float:"left",border:"1px solid",margin:"4px 2px 0px 0px"}} >
+                  <TreeView/>
+                  
+                </div>
+                <div  style= {{float:"right",width:"85%",height:this.state.windowHeight*0.86,border:"1px solid",margin:"4px 0px 0px 2px",overflowY:"auto",overflowX:"hidden"}} >
+               
+                <ProductListView productData={data} wrapperClass="wrapper"
+                    gridClass="col-md-3"
+                    imgClass="responsive"
+                    style= {{width:"100%"}}
+                    perPageProducts = {10}
+                    viewHeight = {this.state.windowHeight*0.926}
+                   />
+                  
+
+                </div>
+               </div>
+          
+      );
+   }
+}
+
+
+
+export default ProductSelectionView
+
